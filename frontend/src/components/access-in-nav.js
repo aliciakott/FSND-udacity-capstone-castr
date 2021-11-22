d2f2c650f6789c52baed8e3b98b5c458774d83e4
alicia kott <amkott@gmail.com>
2021-11-22 13:58:28 -0500

    final updates to main page/navigation

--- frontend/src/components/access-in-nav.js
+++ frontend/src/components/access-in-nav.js
@@ -5,8 +5,7 @@ import LogoutButton from "./logout-button";
 class AccessInNav extends React.Component {
   render() {
     return (
-      <div>
-        <header className="bg-white">
+      <header className="bg-white">
           <Link to="/"><h3 className="float-md-start nav-link text-dark">Castr</h3></Link>
           <nav className="nav nav-masthead justify-content-center float-md-end">
             <Link to="/movies"><h4 className="nav-link text-dark fw-bold">Movies</h4></Link>
@@ -14,7 +13,6 @@ class AccessInNav extends React.Component {
             <LogoutButton />
           </nav>
         </header>
-      </div>
     );
   }
 }
