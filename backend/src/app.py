f8f5f983b4fe4b42b886a8fd008497be47f6dd38
alicia kott <amkott@gmail.com>
2021-11-22 15:13:23 -0500

    added package python-dotenv, and environmental values to .env

--- backend/src/app.py
+++ backend/src/app.py
@@ -33,7 +33,7 @@ def create_app(test_config=None):
     CORS(app)
     @app.after_request
     def after_request(response):
-      response.headers.add('Access-Control-Allow-Origin', '*')
+    #   response.headers.add('Access-Control-Allow-Origin', '*')
       response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
       response.headers.add('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE, OPTIONS')
 
--- backend/src/setup.sh
+++
@@ -1,4 +0,0 @@
-export AUTH0_DOMAIN='coffeeshop-fsnd-udacity.us.auth0.com'
-export ALGORITHMS=['RS256']
-export API_AUDIENCE='castr-api'
-export DATABASE_URL='postgresql://postgres:admin@localhost:5432/castr'
